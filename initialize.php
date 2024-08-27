e9733389ea2daaeb0b702cfaf5f2b8209bbec44b
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-08-27 18:54:40 +0700

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/cscs/');
+if(!defined('base_url')) define('base_url','https://coffee-gdatc5h6hrdnddbp.eastasia-01.azurewebsites.net//');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_SERVER')) define('DB_SERVER',"coffee-shop.mysql.database.azure.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"tuan");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Tohru14617");
 if(!defined('DB_NAME')) define('DB_NAME',"cscs_db");
 ?>
